25885389995baf785cb8f31db16cbfa2d88cad03
eotryx <mhfiedler@gmx.de>
2013-04-28 22:09:12 +0100

    translation uk, ru by Mykyta Solomko

---
+++ l10n/ru.php
@@ -0,0 +1,5 @@
+<?php $TRANSLATIONS = array(
+"Destination directory"=>"Целевой каталог",
+"Move"=>"Переместить",
+"Copy"=>"Скопировать",
+);
---
+++ l10n/uk.php
@@ -0,0 +1,5 @@
+<?php $TRANSLATIONS = array(
+"Destination directory"=>"Цільовий каталог",
+"Move"=>"Перемістити",
+"Copy"=>"Копіювати",
+);
