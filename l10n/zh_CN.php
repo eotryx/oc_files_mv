959739f8dfbbf15ac2a19aee9568e07b5c6ebda1
daiw <daiw@yun70.com.cn>
2014-07-03 14:22:44 +0800

    create zh_CN & zh_TW

---
+++ l10n/zh_CN.php
@@ -0,0 +1,5 @@
+<?php $TRANSLATIONS = array(
+"Destination directory"=>"目标目录",
+"Move"=>"移动",
+"Copy"=>"复制",
+);
---
+++ l10n/zh_TW.php
@@ -0,0 +1,5 @@
+<?php $TRANSLATIONS = array(
+"Destination directory"=>"目標目錄",
+"Move"=>"移動",
+"Copy"=>"複製",
+);
