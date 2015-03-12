19319fa1e025bbf96f53da355a0ec0c9d0b4beeb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-12 00:23:08 -0400

    [tx-robot] updated from transifex

---
+++ l10n/de.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Kein Dateisystem gefunden",
+    "No data supplied." : "Keine Daten angegeben.",
+    "Src and Dest are not allowed to be the same location!" : "Quelle und Ziel dürfen nicht derselbe Ort sein!",
+    "Could not move %s - File with this name already exists" : "Konnte %s nicht verschieben. Eine Datei mit diesem Namen existiert bereits",
+    "Could not move %s" : "Konnte %s nicht verschieben",
+    "Move" : "Verschieben",
+    "Copy" : "Kopieren",
+    "Destination directory" : "Zielordner"
+},
+"nplurals=2; plural=(n != 1);");
