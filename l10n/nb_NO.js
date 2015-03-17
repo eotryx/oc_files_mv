e9337ce11d2c29ceb3ba2effed5178174b50fa7d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-17 00:23:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/nb_NO.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Filsystem ikke funnet",
+    "No data supplied." : "Ingen data angitt.",
+    "Src and Dest are not allowed to be the same location!" : "Kilde og destinasjon kan ikke være like!",
+    "Could not move %s - File with this name already exists" : "Kan ikke flytte %s - En fil med samme navn finnes allerede",
+    "Could not move %s" : "Kunne ikke flytte %s",
+    "Move" : "Flytt",
+    "Copy" : "Kopier",
+    "Destination directory" : "Målkatalog"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/nb_NO.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "Filsystem ikke funnet",
+"No data supplied." => "Ingen data angitt.",
+"Src and Dest are not allowed to be the same location!" => "Kilde og destinasjon kan ikke være like!",
+"Could not move %s - File with this name already exists" => "Kan ikke flytte %s - En fil med samme navn finnes allerede",
+"Could not move %s" => "Kunne ikke flytte %s",
+"Move" => "Flytt",
+"Copy" => "Kopier",
+"Destination directory" => "Målkatalog"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
