9db068b4d3ff2f4cf6ee5e1b46aa88cfd96d726a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-18 00:23:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/pl.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "Brak systemu plików",
+"No data supplied." => "Brak danych",
+"Src and Dest are not allowed to be the same location!" => "Folder źródłowy i docelowy nie mogą być w tej samej lokalizacji",
+"Could not move %s - File with this name already exists" => "Nie można było przenieść %s - Plik o takiej nazwie już istnieje",
+"Could not move %s" => "Nie można było przenieść %s",
+"Move" => "Przenieś",
+"Copy" => "Kopiuj",
+"Destination directory" => "Folder docelowy"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
---
+++ l10n/tr.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Bulunamadı dosya sistemi",
+    "No data supplied." : "Hiçbir veri sağladı.",
+    "Src and Dest are not allowed to be the same location!" : "Src ve Hedef aynı yerde izin verilmez!",
+    "Could not move %s - File with this name already exists" : "%s taşınamadı. Bu isimde dosya zaten mevcut",
+    "Could not move %s" : "%s taşınamadı",
+    "Move" : "Taşı",
+    "Copy" : "Kopyala",
+    "Destination directory" : "Hedef dizini"
+},
+"nplurals=2; plural=(n > 1);");
