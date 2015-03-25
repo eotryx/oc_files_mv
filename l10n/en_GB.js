ec60bf022a77641469116025496f90c6028d0457
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-25 00:23:13 -0400

    [tx-robot] updated from transifex

---
+++ l10n/en_GB.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "No filesystem found",
+    "No data supplied." : "No data supplied.",
+    "Src and Dest are not allowed to be the same location!" : "Src and Dest are not allowed to be the same location!",
+    "Could not move %s - File with this name already exists" : "Could not move %s - File with this name already exists",
+    "Could not move %s" : "Could not move %s",
+    "Move" : "Move",
+    "Copy" : "Copy",
+    "Destination directory" : "Destination directory"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/en_GB.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "No filesystem found",
+"No data supplied." => "No data supplied.",
+"Src and Dest are not allowed to be the same location!" => "Src and Dest are not allowed to be the same location!",
+"Could not move %s - File with this name already exists" => "Could not move %s - File with this name already exists",
+"Could not move %s" => "Could not move %s",
+"Move" => "Move",
+"Copy" => "Copy",
+"Destination directory" => "Destination directory"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
