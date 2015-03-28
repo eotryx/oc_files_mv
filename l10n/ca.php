8e85b3e7ec8e1aa2247e7d8952f82227669e1d60
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-28 00:23:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ca.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "No s'ha trobat cap arxiu del sistema",
+"No data supplied." => "No s'han subministrat les dades.",
+"Src and Dest are not allowed to be the same location!" => "Origen i Destinació no poden tenir la mateixa ubicació!",
+"Could not move %s - File with this name already exists" => "No s'ha pogut moure %s - Ja hi ha un fitxer amb aquest nom",
+"Could not move %s" => " No s'ha pogut moure %s",
+"Move" => "Moure",
+"Copy" => "Còpia",
+"Destination directory" => "Directori de destinació"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
