747a02c831f8f92154d69ab21a1df07859e84a19
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-29 00:23:13 -0400

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -2,7 +2,7 @@
 $TRANSLATIONS = array(
 "No filesystem found" => "Pas de système de fichier",
 "No data supplied." => "Aucune donnée fournie",
-"Src and Dest are not allowed to be the same location!" => "Src et Dest ne peuvent pas avoir le même emplacement",
+"Src and Dest are not allowed to be the same location!" => "Src et Dest ne peuvent pas être le même emplacement!",
 "Could not move %s - File with this name already exists" => "Impossible de déplacer %s - Un fichier portant ce nom existe déjà",
 "Could not move %s" => "Impossible de déplacer %s",
 "Move" => "Déplacer",
