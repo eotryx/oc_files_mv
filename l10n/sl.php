acd38f5be4749563d2811b429b1c44ab98c296cb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-04 00:23:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sl.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "Ni najdenega datotečnega sistema",
+"No data supplied." => "Ni podanih podatkov.",
+"Src and Dest are not allowed to be the same location!" => "Izvorna in ciljna mapa ne moreta biti na istem mestu.",
+"Could not move %s - File with this name already exists" => "Datoteke %s ni mogoče premakniti - datoteka s tem imenom že obstaja.",
+"Could not move %s" => "Datoteke %s ni mogoče premakniti",
+"Move" => "Premakni",
+"Copy" => "Kopiraj",
+"Destination directory" => "Ciljna mapa"
+);
+$PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";
