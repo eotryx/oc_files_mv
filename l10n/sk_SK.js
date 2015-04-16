b3a49ff873d728c6137475057dca915df38f0408
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-16 00:23:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sk_SK.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Súborový systém nenájdený",
+    "Could not move %s - File with this name already exists" : "Nie je možné presunúť %s - súbor s týmto menom už existuje",
+    "Could not move %s" : "Nie je možné presunúť %s",
+    "Move" : "Presunúť",
+    "Copy" : "Kopírovať",
+    "Destination directory" : "Umiestnenie priečinka"
+},
+"nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
---
+++ l10n/sk_SK.php
@@ -0,0 +1,10 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "Súborový systém nenájdený",
+"Could not move %s - File with this name already exists" => "Nie je možné presunúť %s - súbor s týmto menom už existuje",
+"Could not move %s" => "Nie je možné presunúť %s",
+"Move" => "Presunúť",
+"Copy" => "Kopírovať",
+"Destination directory" => "Umiestnenie priečinka"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
