01bf34bb452a013986008effb0fb75cb011c9362
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-05-13 00:23:14 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ko.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "파일 시스템을 찾을 수 없음",
+"No data supplied." => "데이터를 제공하지 않았습니다.",
+"Src and Dest are not allowed to be the same location!" => "원본과 대상 위치가 같을 수 없습니다!",
+"Could not move %s - File with this name already exists" => "항목 %s을(를) 이동시킬 수 없음 - 같은 이름의 파일이 이미 존재함",
+"Could not move %s" => "항목 %s을(를) 이동시킬 수 없음",
+"Move" => "이동",
+"Copy" => "복사",
+"Destination directory" => "대상 디렉터리"
+);
+$PLURAL_FORMS = "nplurals=1; plural=0;";
