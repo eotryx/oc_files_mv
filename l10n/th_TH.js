a6045aa2ea521b7d38d671ca5dd6e86bd284b366
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-06 00:23:15 -0400

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "ไม่พบแฟ้มระบบ",
+    "No data supplied." : "ไม่มีข้อมูลที่ให้มา",
+    "Src and Dest are not allowed to be the same location!" : "Src และ Dest  ไม่ได้รับอนุญาตให้อยู่ที่เดียวกัน!",
+    "Could not move %s - File with this name already exists" : "ไม่สามารถย้าย %s ได้ - ไฟล์ที่ใช้ชื่อนี้มีอยู่แล้ว",
+    "Could not move %s" : "ไม่สามารถย้าย %s ได้",
+    "Move" : "ย้าย",
+    "Copy" : "คัดลอก",
+    "Destination directory" : "ไดเรกทอรีปลายทาง"
+},
+"nplurals=1; plural=0;");
