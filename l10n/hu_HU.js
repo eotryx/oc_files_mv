708122536147d36586fbec0201363f8c642db97a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-08-06 00:23:19 -0400

    [tx-robot] updated from transifex

---
+++ l10n/hu_HU.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Nem található fájlrendszer",
+    "No data supplied." : "Nincs adat megadva.",
+    "Src and Dest are not allowed to be the same location!" : "Src és Dest nem lehet ugyanazon a helyen!",
+    "Could not move %s - File with this name already exists" : "%s áthelyezése nem sikerült - már létezik másik fájl ezzel a névvel",
+    "Could not move %s" : "Nem sikerült %s áthelyezése",
+    "Move" : "Mozgatás",
+    "Copy" : "Másolás",
+    "Destination directory" : "Cél könyvtár"
+},
+"nplurals=2; plural=(n != 1);");
