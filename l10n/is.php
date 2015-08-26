dee89871018bf90459ae7b8cfba6131b55d92399
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-08-26 00:23:19 -0400

    [tx-robot] updated from transifex

---
+++ l10n/is.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "Ekkert skráakerfi fannst",
+"No data supplied." => "Engar upplýsingar fylgja.",
+"Src and Dest are not allowed to be the same location!" => "Upphafs og áfangastaður meiga ekki vera það sama!",
+"Could not move %s - File with this name already exists" => "Gat ekki fært %s - Skrá með þessu nafni er þegar til",
+"Could not move %s" => "Gat ekki fært %s",
+"Move" => "Færa",
+"Copy" => "Afrita",
+"Destination directory" => "Áfangastaðs mappa"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);";
