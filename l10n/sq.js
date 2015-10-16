301db782be8cd601fa52f784de67cf5658b346b0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-16 00:23:16 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sq.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "S’u gjet sistem kartelash",
+    "No data supplied." : "S’u dhanë të dhëna.",
+    "Src and Dest are not allowed to be the same location!" : "Burimi dhe Vendmbërritja nuk lejohet të jenë i njëjti vend!",
+    "Could not move %s - File with this name already exists" : "E pa mundur zhvendosja e %s - ekziston nje skedar me te njetin emer",
+    "Could not move %s" : "Nuk mund të zhvendoset %s",
+    "Move" : "Zhvendose",
+    "Copy" : "Kopjoje",
+    "Destination directory" : "Drejtori vendmbërritje"
+},
+"nplurals=2; plural=(n != 1);");
