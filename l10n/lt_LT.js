4c69987acce87c2c56be95a3e8fe78a2121dc148
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-11-04 00:23:19 -0500

    [tx-robot] updated from transifex

---
+++ l10n/lt_LT.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Failų sistema nerasta",
+    "No data supplied." : "Nepateikti duomenys",
+    "Src and Dest are not allowed to be the same location!" : "Šaltinis ir paskirtis negali būti vienodi!",
+    "Could not move %s - File with this name already exists" : "Nepavyko perkelti %s - failas su tokiu pavadinimu jau egzistuoja",
+    "Could not move %s" : "Nepavyko perkelti %s",
+    "Move" : "perkelti",
+    "Copy" : "Kopijuoti",
+    "Destination directory" : "Paskirties aplankas"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);");
