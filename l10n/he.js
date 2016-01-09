27c56a86f0dc06f2d5bee9e5b0dfd0cbbdc6a15b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-09 00:23:15 -0500

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "לא נמצאו קובצי מערכת",
+    "No data supplied." : "לא סופק מידע.",
+    "Src and Dest are not allowed to be the same location!" : "מקור ויעד לא יכולים להיות באותו מיקום!",
+    "Could not move %s - File with this name already exists" : "לא ניתן להעביר את %s - קובץ בשם הזה כבר קיים",
+    "Could not move %s" : "לא ניתן להעביר את %s",
+    "Move" : "העברה",
+    "Copy" : "העתקה",
+    "Destination directory" : "תיקיית יעד"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/he.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"No filesystem found" => "לא נמצאו קובצי מערכת",
+"No data supplied." => "לא סופק מידע.",
+"Src and Dest are not allowed to be the same location!" => "מקור ויעד לא יכולים להיות באותו מיקום!",
+"Could not move %s - File with this name already exists" => "לא ניתן להעביר את %s - קובץ בשם הזה כבר קיים",
+"Could not move %s" => "לא ניתן להעביר את %s",
+"Move" => "העברה",
+"Copy" => "העתקה",
+"Destination directory" => "תיקיית יעד"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
