d0483cad102baa076f9bba915fb19997ae805add
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-02-01 00:23:15 +0100

    [tx-robot] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -3,9 +3,11 @@ OC.L10N.register(
     {
     "No filesystem found" : "Не е открита файлова система",
     "No data supplied." : "Не са посочени данни",
+    "Src and Dest are not allowed to be the same location!" : "Src и Dest не може да бъдат същото местоположение!",
     "Could not move %s - File with this name already exists" : "Неуспешно преместване на %s - Файл със същото име вече съществува.",
     "Could not move %s" : "Неуспешно преместване на %s.",
     "Move" : "Премести",
-    "Copy" : "Копирай"
+    "Copy" : "Копирай",
+    "Destination directory" : "Крайна папка"
 },
 "nplurals=2; plural=(n != 1);");
