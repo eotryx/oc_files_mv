647ef2fdd8ca1c1fcb0737dd073c72d219555ae3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-02-11 00:23:17 +0100

    [tx-robot] updated from transifex

--- l10n/uk.js
+++ l10n/uk.js
@@ -2,12 +2,12 @@ OC.L10N.register(
     "oc_files_mv",
     {
     "No filesystem found" : "Не знайдено жодної файлової системи",
-    "No data supplied." : "Дані не вказано.",
-    "Src and Dest are not allowed to be the same location!" : "Джерело та призначення не можуть бути одним й тим же розміщенням!",
+    "No data supplied." : "Немає даних",
+    "Src and Dest are not allowed to be the same location!" : "Джерело та призначення не можуть бути одним й тим самим!",
     "Could not move %s - File with this name already exists" : "Не вдалося перемістити %s - Файл з таким ім'ям вже існує",
     "Could not move %s" : "Не вдалося перемістити %s",
     "Move" : "Перемістити",
     "Copy" : "Копіювати",
-    "Destination directory" : "Кінцевий каталог"
+    "Destination directory" : "Тека призначення"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
