402502ab6604e6286459c0dae39777135cecdc31
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-06 01:40:24 +0000

    [tx-robot] updated from transifex

--- l10n/sv.js
+++ l10n/sv.js
@@ -3,9 +3,11 @@ OC.L10N.register(
     {
     "No filesystem found" : "Kunde inte hitta något filsystem",
     "No data supplied." : "Ingen data levererades.",
+    "Src and Dest are not allowed to be the same location!" : "Källa och destination kan inte vara samma plats!",
     "Could not move %s - File with this name already exists" : "Kunde inte flytta %s - Det finns redan en fil med detta namn",
     "Could not move %s" : "Kan inte flytta %s",
     "Move" : "Flytta",
-    "Copy" : "Kopiera"
+    "Copy" : "Kopiera",
+    "Destination directory" : "Destinationsmapp"
 },
 "nplurals=2; plural=(n != 1);");
