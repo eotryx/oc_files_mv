c94a76acaf91e31d28b586a8d0c4db83e9cc752c
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-24 01:40:44 +0000

    [tx-robot] updated from transifex

---
+++ l10n/es_AR.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Ningún sistema de archivos encontrado",
+    "Src and Dest are not allowed to be the same location!" : "El orígen y destino no ser la misma ubicación!",
+    "Could not move %s - File with this name already exists" : "No se pudo mover %s - Un archivo con este nombre ya existe",
+    "Could not move %s" : "No se pudo mover %s ",
+    "Move" : "Mover",
+    "Copy" : "Copiar"
+},
+"nplurals=2; plural=(n != 1);");
