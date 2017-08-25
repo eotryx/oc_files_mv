f2ef906a331bf891de54f25b11d5d0616ee56610
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-25 01:40:26 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Geen lêerstelsel gevind",
+    "No data supplied." : "Geen data verskaf.",
+    "Src and Dest are not allowed to be the same location!" : "Bron en Bestemming mag nie dieselfde plek wees nie!",
+    "Could not move %s - File with this name already exists" : "Kon nie %s skuif nie - Lêer met hierdie naam bestaan reeds",
+    "Could not move %s" : "Kon nie %s skuif nie",
+    "Move" : "Skuif",
+    "Copy" : "Kopieer",
+    "Destination directory" : "Bestemmingsgids"
+},
+"nplurals=2; plural=(n != 1);");
